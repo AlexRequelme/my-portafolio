639d273d692696c0d6ab9058f4ab147e6608f53b
AlexRequelme <alexdeveloper09@gmail.com>
2021-08-24 15:59:07 -0500

    Diseño basico

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import { library } from '@fortawesome/fontawesome-svg-core';
+import projects from './assets/projects.json';
+import skills from './assets/skills.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full py-8 bg-gray-800 text-gray-300 flex flex-col items-center justify-center">
+      <div>
+        Alex Requelme -
+        <span>Software engineer</span>
+        <img src="https://img.icons8.com/cotton/64/000000/developer.png"/>
+      </div>
+      <section className="w-1/2 mb-4">
+        <h1 className="text-xl text-blue-500">About</h1>
+        <div className="ml-4 mt-4">
+          <p className="mb-2">I am a young Ecuadorian who likes to solve problems and improve his skills at all times.</p>
+          <p className="mb-2">I have a degree in software engineering.</p>
+        </div>        
+      </section>
+      <section className="w-1/2 mb-4">
+        <h1 className="text-xl text-blue-500">Skills</h1>
+        <div className="grid grid-flow-row grid-cols-3 gap-4 mt-4 ml-4">
+          {skills.map((s, i) => (
+            <div key={i} className="flex items-center">
+              <img src={s.icon} />
+              <span className="ml-2">{s.name}</span>
+            </div>
+          ))}
+        </div>        
+      </section>
+      <section className="w-1/2">
+        <h1 className="text-xl text-blue-500">Projects</h1>
+        <div className="ml-4 mt-4">
+          { projects.map((p, i) => (
+              <div key={i} className="mb-8">
+                <div className="flex items-center mb-2">
+                  <h2 className="text-lg text-blue-500 mr-2">{p.name}</h2>
+                  <a href={p.url} target="_blank">
+                    <img src="https://img.icons8.com/windows/28/ffffff/external-link-squared.png"/>
+                  </a>
+                </div>
+                
+                <p>{p.desc}</p>
+                
+                <div className="flex mt-4">
+                  {p.technologies.map((t, i) => {
+                    return (
+                      <div key={i} className="mr-4 bg-gray-700 px-4 py-1 rounded-full">
+                        {t}
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
